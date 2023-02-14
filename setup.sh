44874e81b7b2b8d925726dad42804ad941eb7ea1
Fabiano Soares Martins de Alencar <fabiano.alencar@dataprev.gov.br>
2023-02-14 19:07:59 -0300

    v1

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+NAMESPACE=kube-datalake
+
+kubectl create namespace $NAMESPACE
+kubectl create secret generic my-s3-keys --from-literal=access-key='minio' --from-literal=secret-key='minio123' -n $NAMESPACE
+
+
+
+kubectl apply -n $NAMESPACE -f mariadb/maria_pvc.yaml
+kubectl apply -n $NAMESPACE -f mariadb/maria_deployment.yaml
+
+
+
+kubectl apply -n $NAMESPACE -f hive_metastore/hive-initschema.yaml
+
+kubectl create configmap metastore-cfg --dry-run --from-file=hive_metastore/metastore-site.xml | kubectl apply -n datalake2  -f -
+kubectl apply -n $NAMESPACE -f hive_metastore/metastore.yaml
+
+
+kubectl apply -n $NAMESPACE -f trino/trino-cfgs.yaml
+kubectl apply -n $NAMESPACE -f trino/trino.yaml
