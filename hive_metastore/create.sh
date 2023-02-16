74ba931b0f1e86981a07f8c808309d6b3179d6b6
Fabiano Soares Martins de Alencar <fabiano.alencar@dataprev.gov.br>
2023-02-16 09:34:44 -0300

    Refactory 1

---
+++ hive_metastore/create.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+# Chamada create.sh <NAMESPACE>
+
+set -e
+
+NAMESPACE=$1
+
+../mariadb/create.sh $NAMESPACE
+
+# create metastore db schemas.
+kubectl apply -n $NAMESPACE -f hive-initschema.yaml
+
+# wait for finishing creating schemas.
+while [[ $(kubectl get pods -n $NAMESPACE -l job-name=hive-initschema -o jsonpath={..status.phase}) != *"Succeeded"* ]]; do echo "waiting for finishing init schema job" && sleep 2; done
+
+#kubectl create configmap metastore-cfg -n $NAMESPACE  --from-file=metastore-site.xml  -o yaml | kubectl apply -n $NAMESPACE -f -
+
+kubectl apply -n $NAMESPACE -f metastore-cfg.yaml
+kubectl apply -n $NAMESPACE -f metastore.yaml
