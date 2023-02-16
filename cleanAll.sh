ded42a79105744189c0dcf75b32251ffb2369b72
Fabiano Alencar <fabiano.alencar@gmail.com>
2023-02-16 12:09:45 -0300

    Refactory 1

--- cleanAll.sh
+++ cleanAll.sh
@@ -6,5 +6,6 @@ set -e
 
 NAMESPACE=$1
 
-kubectl delete namespace $NAMESPACE
+kubectl delete namespace $NAMESPACE &
+sleep 1m
 kubectl get namespace $NAMESPACE -o json | jq 'del(.spec.finalizers[0])' | kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f -
--- setup.sh
+++ setup.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+# Chamada create.sh <NAMESPACE>
+
 set -e
 
-NAMESPACE=kube-datalake
+NAMESPACE=$1
 
 kubectl create namespace $NAMESPACE
+
 kubectl create secret generic my-s3-keys --from-literal=access-key='minio' --from-literal=secret-key='minio123' -n $NAMESPACE
 
 
+cd mariadb
 
-kubectl apply -n $NAMESPACE -f mariadb/maria_pvc.yaml
-kubectl apply -n $NAMESPACE -f mariadb/maria_deployment.yaml
+sh create.sh $NAMESPACE
 
-kubectl apply -n $NAMESPACE -f hive_metastore/hive-initschema.yaml
+cd ..
+cd hive_metastore
 
-kubectl create configmap metastore-cfg -n $NAMESPACE  --from-file=hive_metastore/metastore-site.xml  -o yaml | kubectl apply -n $NAMESPACE -f -
+sh create.sh $NAMESPACE
 
-kubectl apply -n $NAMESPACE -f hive_metastore/metastore.yaml
-
+cd ..
 
 kubectl apply -n $NAMESPACE -f trino/trino-cfgs.yaml
 kubectl apply -n $NAMESPACE -f trino/trino.yaml
+
+
