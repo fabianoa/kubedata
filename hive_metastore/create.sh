a138fb11ad0d7c9b924377d4ac976c878af54097
Fabiano Soares Martins de Alencar <fabiano.alencar@dataprev.gov.br>
2023-02-21 18:46:56 -0300

    Add Presto

--- hive_metastore/create.sh
+++ hive_metastore/create.sh
@@ -13,8 +13,6 @@ kubectl apply -n $NAMESPACE -f hive-initschema.yaml
 # wait for finishing creating schemas.
 while [[ $(kubectl get pods -n $NAMESPACE -l job-name=hive-initschema -o jsonpath={..status.phase}) != *"Succeeded"* ]]; do echo "waiting for finishing init schema job" && sleep 2; done
 
-#kubectl create configmap metastore-cfg -n $NAMESPACE  --from-file=metastore-site.xml  -o yaml | kubectl apply -n $NAMESPACE -f -
-
 kubectl apply -n $NAMESPACE -f metastore-cfg.yaml
 kubectl apply -n $NAMESPACE -f metastore.yaml
 
