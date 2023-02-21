a4af49f85b1450c7418b1ac372affeb5bfc8ddbd
Fabiano Soares Martins de Alencar <fabiano.alencar@dataprev.gov.br>
2023-02-21 15:03:03 -0300

    Add Ranger

--- ranger-admin/create.sh
+++ ranger-admin/create.sh
@@ -8,7 +8,7 @@ set -e
 NAMESPACE=$1
 
 kubectl apply -n $NAMESPACE -f ranger-cfg.yaml
-kubectl apply -n $NAMESPACE -f ranger-addmin.yaml
+kubectl apply -n $NAMESPACE -f ranger-admin.yaml
 
 # wait for mysql pod being ready.
 while [[ $(kubectl get pods -n $NAMESPACE -l app=ranger-admin -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for ranger-admin pod being ready" && sleep 1; done
