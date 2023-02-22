654d9fdecc3ce87c1be64ff4d5baecb59a07a4cd
Fabiano Soares Martins de Alencar <fabiano.alencar@dataprev.gov.br>
2023-02-22 10:41:55 -0300

    Add Openldap

--- openldap/create.sh
+++ openldap/create.sh
@@ -9,4 +9,4 @@ NAMESPACE=$1
 kubectl apply -n $NAMESPACE -f openldap.yaml
 
 # wait for mysql pod being ready.
-while [[ $(kubectl get pods -n $NAMESPACE -l app=openldap -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for openldap pod being ready" && sleep 1; done
+while [[ $(kubectl get pods -n $NAMESPACE -l app=openldap-srv -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for openldap-srv pod being ready" && sleep 1; done
