48aa65db49989ea0324ee2e9ae34e2dcc41f7462
Fabiano Soares Martins de Alencar <fabiano.alencar@dataprev.gov.br>
2023-02-27 20:15:33 -0300

    Add Zeppelin conf

--- zeppelin/create.sh
+++ zeppelin/create.sh
@@ -7,14 +7,8 @@ set -e
 
 NAMESPACE=$1
 
-# create metastore db schemas.
-kubectl apply -n $NAMESPACE -f hive-initschema.yaml
-
-# wait for finishing creating schemas.
-while [[ $(kubectl get pods -n $NAMESPACE -l job-name=hive-initschema -o jsonpath={..status.phase}) != *"Succeeded"* ]]; do echo "waiting for finishing init schema job" && sleep 2; done
-
-kubectl apply -n $NAMESPACE -f metastore-cfg.yaml
-kubectl apply -n $NAMESPACE -f metastore.yaml
+kubectl apply -n $NAMESPACE -f zeppelin-cfg.yaml
+kubectl apply -n $NAMESPACE -f zeppelin-server.yaml
 
 # wait for mysql pod being ready.
-while [[ $(kubectl get pods -n $NAMESPACE -l app=metastore -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for metastore pod being ready" && sleep 1; done
+while [[ $(kubectl get pods -n $NAMESPACE -l app=zeppelin-server -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for zeppelin-server pod being ready" && sleep 1; done
